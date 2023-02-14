3ccda3f6c3fde9315e6bb429353a89dc942d8022
Sunshine Richmer Palahang <sunshinerichmerdpalahang@iskolarngbayan.pup.edu.ph>
2023-02-14 15:48:02 +0800

    adding font color

--- Modified-Job-Scheduler/job_scheduler.py
+++ Modified-Job-Scheduler/job_scheduler.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from bst_demo import BSTDemo, Node
+from colorama import Fore, Style
 
 def get_job_input_details():
     start_time = input("Enter the time in hh:mm format, example 18:30 or 6:30-> ")
@@ -40,15 +41,20 @@ my_tree = BSTDemo()
 with open("Modified-Job-Scheduler/data.txt") as f:
     for line in f:
         my_tree.insert(line)
+def menu():
+    print(Fore.BLUE + "\n<--------------------TODAYS TASK-------------------->")
+    print(Style.RESET_ALL)
+    print("Please choose an option from the list below:")
+    print(Fore.CYAN+"Press 1 to view today's scheduled tasks")
+    print(Fore.BLUE+"Press 2 to add a task to today's schedule")
+    print(Fore.GREEN+"Press 3 to remove a task from the schedule")
+    print(Fore.MAGENTA+"Press 4 to search a task from the schedule")
+    print(Fore.YELLOW+"Press 5 to set status of a task")
+    print(Fore.RED+"Press 6 to quit")
+    print(Style.RESET_ALL)
 
 while True:
-    print("Please choose an option from the list below:")
-    print("Press 1 to view today's scheduled tasks")
-    print("Press 2 to add a task to today's schedule")
-    print("Press 3 to remove a task from the schedule")
-    print("Press 4 to search a task from the schedule")
-    print("Press 5 to set status of a task")
-    print("Press 6 to quit")
+    menu()
     selection = input("Enter your choice-> ")
     try:
         entry = int(selection)
@@ -58,7 +64,8 @@ while True:
     if int(selection) == 1:
         my_tree.in_order()
     elif int(selection) == 2:
-        print("You have chosen to add a task to the schedule")
+        print(Fore.BLUE+"You have chosen to add a task to the schedule")
+        print(Style.RESET_ALL)
         start_time, duration_of_job, job_name = get_job_input_details()
         line = start_time+","+duration_of_job+","+job_name
         num = my_tree.length()
@@ -68,16 +75,18 @@ while True:
                 to_write.write(line+"\n")
         input("Press any key to continue... ")
     elif int(selection) == 3:
-        print("You have chosen to remove a task from the schedule")
+        print(Fore.GREEN + "You have chosen to remove a task from the schedule")
+        print(Style.RESET_ALL)
         start_time, duration_of_job, job_name = get_job_input_details()
         key_to_find = datetime.strptime(start_time, '%H:%M').time()
         result = my_tree.find_val(key_to_find)
         if result:
             if result.name_of_job == job_name and result.duration == duration_of_job:
-                print("Removing task:")
+                print(Fore.GREEN+"Removing task:")
                 print(result)
                 my_tree.delete_val(key_to_find)
                 print("Task successfully removed")
+                print(Style.RESET_ALL)
                 with open("Modified-Job-Scheduler/data.txt", "r") as f:
                     lines = f.readlines()
                 with open("Modified-Job-Scheduler/data.txt", "w") as f:
@@ -93,13 +102,15 @@ while True:
             input("Press any key to continue... ")
     #add search feature
     elif int(selection) == 4:
-        print("You have chosen to search a task from the schedule")
+        print(Fore.MAGENTA + "You have chosen to search a task from the schedule")
+        print(Style.RESET_ALL)
         start_time= search_job()
         key_to_find = datetime.strptime(start_time, '%H:%M').time()
         result = my_tree.find_val(key_to_find)
         if result:
-            print("Searching task:")
+            print(Fore.MAGENTA+"Searching task:")
             print(result)
+            print(Style.RESET_ALL)
             with open("Modified-Job-Scheduler/data.txt", "r") as f:
                 lines = f.readlines()
             input("Press any key to continue... ")
@@ -108,18 +119,20 @@ while True:
             input("Press any key to continue... ")     
     #add set status feature       
     elif int(selection) == 5:
-        print("You have chosen to set a status a task from the schedule")
+        print(Fore.YELLOW+"You have chosen to set a status a task from the schedule")
+        print(Style.RESET_ALL)
         start_time, duration_of_job, job_name = get_job_input_details()
         key_to_find = datetime.strptime(start_time, '%H:%M').time()
         result = my_tree.find_val(key_to_find)
         if result:
             if result.name_of_job == job_name and result.duration == duration_of_job:
-                print("Searching task:")
+                print(Fore.YELLOW+"Searching task:")
                 print(result)
                 status = input("Accomplished? (y/n): ")
                 print(result, "DONE")
                 my_tree.delete_val(key_to_find)
                 print("Task successfully accomplished!")
+                print(Style.RESET_ALL)
                 with open("Modified-Job-Scheduler/data.txt", "r") as f:
                     lines = f.readlines()
                 with open("Modified-Job-Scheduler/data.txt", "w") as f:
@@ -134,7 +147,7 @@ while True:
             print("Task not found")
             input("Press any key to continue... ")            
     elif int(selection) == 6:
-        print("Exiting program...")
+        print(Fore.RED+"Exiting program...")
         break
     else:
         print("Please enter a number between 1 to 6")
